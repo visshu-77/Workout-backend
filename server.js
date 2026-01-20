3b860526e12a266890a3e97a0625796442ddb456
visshu-77 <vishal676570@gmail.com>
2026-01-20 17:47:14 +0530

    Configure CORS middleware for localhost access
    Reintroduced CORS middleware with specific settings for localhost.

--- server.js
+++ server.js
@@ -1,7 +1,15 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const cors = require("cors");
+// const cors = require("cors");
 require("dotenv").config();
+
+const cors = require("cors");
+
+app.use(cors({
+origin: ["http://localhost:5173"],
+methods: ["GET", "POST", "PUT", "DELETE"],
+credentials: true
+}));
 
 const authRoutes = require("./routes/authRoutes");
 const workoutRoutes = require("./routes/workoutRoutes");
@@ -9,7 +17,7 @@ const workoutRoutes = require("./routes/workoutRoutes");
 const app = express();
 
 // Middleware
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
