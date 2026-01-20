4204c199fddbe91e66e1e9a9b58c1d06eec370f1
visshu-77 <vishal676570@gmail.com>
2026-01-20 18:28:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,8 @@ app.use("/api/workout", workoutRoutes);
 
 // MongoDB Connection
 mongoose
-  .connect(process.env.MONGO_URI)
+  // .connect(process.env.MONGO_URI)
+  .connect("mongodb+srv://vishalchoudhary98760_db_user:vishal676570@cluster0.hfbm3lw.mongodb.net/?appName=Cluster0")
   .then(() => console.log("✅ MongoDB Connected"))
   .catch(err => console.log("❌ MongoDB Error:", err));
 
