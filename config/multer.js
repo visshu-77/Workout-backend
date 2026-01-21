d846214c8aff6475c32910fed1bdc24177644c54
visshu-77 <vishal676570@gmail.com>
2026-01-21 15:14:43 +0530

    version updates 1.0

--- config/multer.js
+++ config/multer.js
@@ -1,18 +1,39 @@
+// const multer = require("multer");
+// const path = require("path");
+
+// const storage = multer.diskStorage({
+//   destination: (req, file, cb) => {
+//     cb(null, "uploads/");
+//   },
+//   filename: (req, file, cb) => {
+//     cb(
+//       null,
+//       Date.now() + path.extname(file.originalname)
+//     );
+//   }
+// });
+
+// const upload = multer({ storage });
+
+// module.exports = upload;
+
 const multer = require("multer");
-const path = require("path");
 
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, "uploads/");
+// ✅ MEMORY storage (required for serverless)
+const storage = multer.memoryStorage();
+
+const upload = multer({
+  storage,
+  // limits: {
+  //   fileSize: 10 * 1024 * 1024, // 5MB limit
+  // },
+  fileFilter: (req, file, cb) => {
+    if (file.mimetype.startsWith("image/")) {
+      cb(null, true);
+    } else {
+      cb(new Error("Only image files are allowed"), false);
+    }
   },
-  filename: (req, file, cb) => {
-    cb(
-      null,
-      Date.now() + path.extname(file.originalname)
-    );
-  }
 });
-
-const upload = multer({ storage });
 
 module.exports = upload;
