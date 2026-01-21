213e04b72e3264b90427572e981cf8370b36709e
visshu-77 <vishal676570@gmail.com>
2026-01-21 16:48:36 +0530

    updated

--- config/multer.js
+++ config/multer.js
@@ -24,10 +24,10 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const storage = multer.memoryStorage();
 
 const upload = multer({
-  storage :CloudinaryStorage,
-  // limits: {
-  //   fileSize: 10 * 1024 * 1024, // 5MB limit
-  // },
+  storage,
+  limits: {
+    fileSize: 10 * 1024 * 1024, // 5MB limit
+  },
   fileFilter: (req, file, cb) => {
     if (file.mimetype.startsWith("image/")) {
       cb(null, true);
