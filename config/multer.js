294f8d4bfc00c52e8e7ec8d45bce82a1ca16140e
visshu-77 <vishal676570@gmail.com>
2026-01-21 15:52:20 +0530

    cloudinary setup

--- config/multer.js
+++ config/multer.js
@@ -14,7 +14,7 @@ const storage = multer.diskStorage({
 });
 
 
-const MAX_FILE_SIZE = 2 * 1024 * 1024; 
+const MAX_FILE_SIZE = 2 * 1024 * 1024;
 const upload = multer({ storage, limits: { fileSize: MAX_FILE_SIZE } });
 
 module.exports = upload;
